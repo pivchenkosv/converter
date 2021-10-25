1088d1a43e6e7d63b1361260d6a633db5419cd87
stanislau <stanislau.piuchanka@itechart-group.com>
2021-10-25 21:26:06 +0300

    configure swagger

---
+++ app/Http/Controllers/HealthCheck.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HealthCheck extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/healthcheck",
+     *     @OA\Response(response="200", description="Health check endpoint.")
+     * )
+     */
+    public function healthCheck()
+    {
+        return response('OK', 200);
+    }
+}
