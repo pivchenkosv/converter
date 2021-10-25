497cb4f72a8cd5c16ddbb62e0a9327f58ad60d2d
stanislau <stanislau.piuchanka@itechart-group.com>
2021-10-25 20:20:32 +0300

    configure Docker containers

---
+++ docker/worker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+php ~/artisan queue:work --verbose --tries=3 --timeout=90 &
+
+while [ true ]
+    do
+      php ~/artisan schedule:run --verbose --no-interaction &
+      sleep 60
+    done
