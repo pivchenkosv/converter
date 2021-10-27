a75624414384dbb30943317d53c2170eb7ebb4b6
stanislau <stanislau.piuchanka@itechart-group.com>
2021-10-27 21:47:00 +0300

    exchangerate api client

---
+++ app/ApiClients/Exchangerate/ExchangeRateApi.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\ApiClients\Exchangerate;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+
+class ExchangeRateApi
+{
+    private string $baseUrl = 'https://api.exchangerate.host';
+
+    public function convert(string $from, string $to, string $amount): Response
+    {
+        return Http::get("{$this->baseUrl}/convert", [
+            'from' => $from,
+            'to' => $to,
+            'amount' => $amount,
+        ]);
+    }
+}
